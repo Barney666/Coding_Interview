92ccd5b91fb82b4be708110bbf193f3de768c7e0
Barney <540747702@qq.com>
2020-07-22 08:49:47 +0800

    第45题完成

---
+++ src/SwordToOffer/Exercise45.java
@@ -0,0 +1,44 @@
+package SwordToOffer;
+
+import java.util.Arrays;
+
+/**
+ * 扑克牌顺子
+ *
+ * */
+public class Exercise45 {
+    public boolean isContinuous(int [] numbers) {
+        if(numbers.length==0) return false;
+        Arrays.sort(numbers);     // java也有！用的是快排
+        int replace=0;
+        int first=-1;
+        int firstIndex=-1;
+        for(int i=0;i<numbers.length;i++){
+            int num=numbers[i];
+            if(num==0)
+                replace+=1;
+            else{
+                if(first==-1){
+                    first=num;
+                    firstIndex=i;
+                }
+                else{
+                    if(num==numbers[i-1]) return false;   // 只要有重复的肯定就不行了
+                    if(i-firstIndex!=num-first)
+                        if(replace>0)
+                            replace-=(num-numbers[i-1]-1);   // 减去和前一个数字相差的大小
+                        else
+                            return false;
+                }
+            }
+        }
+        return replace<0 ? false : true;
+    }
+}
+class x{
+    public static void main(String[] args) {
+        Exercise45 exercise45=new Exercise45();
+        int[] a= {0,3,2,6,4};
+        exercise45.isContinuous(a);
+    }
+}

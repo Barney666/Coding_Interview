014ef4a1a922771abd93a4bb2addf7f07cdf8823
Barney <540747702@qq.com>
2020-09-08 00:22:27 +0800

    第21题万层

---
+++ src/SwordToOffer/Exercise20.java
@@ -0,0 +1,24 @@
+package SwordToOffer;
+
+/**
+ * 定义一个包含min函数的栈【min函数时间复杂度为o(1)】
+ *
+ * */
+public class Exercise20 {
+
+    public void push(int node) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int min() {
+
+    }
+}

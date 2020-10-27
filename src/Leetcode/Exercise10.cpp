30967632d2184f7ce18b9437b18cbe0371f7ad52
Barney <540747702@qq.com>
2020-10-27 21:02:15 +0800

    通配符匹配（动态规划）

--- src/Leetcode/Exercise10.cpp
+++ src/Leetcode/Exercise10.cpp
@@ -10,7 +10,7 @@ public:
         int p_length = p.size();
         bool dp[s_length+1][p_length+1];    // dp[i][j]表示字符串s的前i个字符能否与模式p前j个字符相匹配
         //注意是前i个和前j个，不是index为i/j，因此后面在s/p上用i/j时要减1
-        memset(dp, false, sizeof(dp));    // dp[i][0]为false，dp[0][j]只要p[j-1]不是*就是false
+        memset(dp, false, sizeof(dp));    // dp[i][0]为false，dp[0][j]只要不是p[j]*就是false
         // memset是对连续空间赋值，所以只能用于直接定义的二维数组,因为[][]这样定义内存是连续的，嵌套一维那种定义就要定义一次一维memset一次
         dp[0][0] = true;
         int index = 1;
@@ -20,10 +20,10 @@ public:
         }
         for(int i = 1; i < s_length + 1; i++){
             for(int j = 1; j < p_length + 1; j++){
-                if(s[i-1]==p[j-1] || p[j-1]=='?')
+                if(s[i-1]==p[j-1] || p[j-1]=='.')
                     dp[i][j] = dp[i-1][j-1];
                 else if(p[j-1] == '*')
-                    dp[i][j] = (dp[i][j-1] || dp[i-1][j]);    // 不使用'*' 或 使用'*'
+                    dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
             }
         }
         return dp[s_length][p_length];

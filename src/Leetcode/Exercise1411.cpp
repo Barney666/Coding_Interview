ba74630cede2820ecc362d8ed71edabf40614cb6
Barney <540747702@qq.com>
2021-03-07 23:08:41 +0800

    给 N x 3 网格图涂色的方案数（数学规律，阿里笔试第一题）

--- src/Leetcode/Exercise1411.cpp
+++ src/Leetcode/Exercise1411.cpp
@@ -1,4 +1,14 @@
-//
-// Created by 苗轶轩 on 2021/3/7.
-//
-
+class Solution {
+public:
+    int numOfWays(int n) {
+        int mod = 1000000007;       // 最好别写10^9+7
+        int arr[n][2];
+        arr[0][0] = 6;
+        arr[0][1] = 6;
+        for(int i = 1; i < n; i++){
+            arr[i][0] = (2l * arr[i-1][0] + 2l * arr[i-1][1]) % mod;        // 这里2和3要用long，然后再转int？不然会溢出
+            arr[i][1] = (2l * arr[i-1][0] + 3l * arr[i-1][1]) % mod;
+        }
+        return (arr[n-1][0] + arr[n-1][1]) % mod;
+    }
+};
